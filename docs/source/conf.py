09702a36cfe768030a5c8a00afa51f6822a598d5
jinningwang <jinninggm@gmail.com>
2025-06-05 20:49:24 -0400

    Fix RTD integration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,16 +43,12 @@ latex_engine = "xelatex"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "use_edit_page_button": True,
-}
 
 html_context = {
     "github_url": "https://github.com",
