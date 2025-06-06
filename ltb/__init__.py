f86a9ceb583f76c8e203e9691c44835ab73e97db
jinningwang <jinninggm@gmail.com>
2025-06-05 20:28:28 -0400

    Remove python packing

--- ltb/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import _version
-__version__ = _version.get_versions()['version']
-
-__author__ = 'CURENT LTB'
-
-__all__ = ['main', 'cli', '__version__']
--- ltb/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-LTB main entry point Redirection to main.py
-This makes the package callable with python -m ltb
-"""
-
-from ltb.cli import main
-
-if __name__ == '__main__':
-    main()
